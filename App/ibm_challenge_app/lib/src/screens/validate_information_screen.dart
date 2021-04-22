5ac48d974edf6f2978a70a4be7f8ef5668201c3b
PatoSantos <pato.santos10@gmail.com>
2021-04-22 14:26:07 -0500

    Improved Validate Information Screen interface
    Gave structure to file and added TakePhotoComponents

--- App/ibm_challenge_app/lib/src/screens/validate_information_screen.dart
+++ App/ibm_challenge_app/lib/src/screens/validate_information_screen.dart
@@ -1,30 +1,62 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Validación de elector'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(
-            children: <Widget>[
-              Text("Necesitamos una foto de su INE y de usted para validar"
-                  " su identidad"
-              ),
-              ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Upload Image"))
-            ],
-          )
-      ),
-      bottomSheet: ElevatedButton(
-          onPressed: () {},
-          child: Text("Continuar")
+import 'components/TakePhotoComponent.dart';
 
+class ValidateInformationScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _ValidateInformationScreen();
+
+}
+
+class _ValidateInformationScreen extends State<ValidateInformationScreen>{
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          margin: EdgeInsets.only(top: 30),
+          child: ListView(
+            children: [
+              Text("Necesitamos información para saber si puedes votar en "
+                  "estas elecciones",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left:15.0, right: 15.0, bottom: 15.0, top: 50.0),
+                child: Text("Sube aquí una foto de tu INE",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              TakePhotoComponent(isNew: false),
+
+              Container(
+                margin: EdgeInsets.only(left:15.0, right: 15.0, bottom: 15.0, top: 50.0),
+                child: Text("Necesitamos una foto tuya para validar tu "
+                    "identidad",
+                    style: TextStyle()),
+              ),
+              TakePhotoComponent(isNew: true)
+            ],
+          ),
+        ),
       ),
-    ),
-  ),
-  );
+      bottomNavigationBar: ElevatedButton(
+        style: ButtonStyle(
+            padding: MaterialStateProperty.all(EdgeInsets.only(top: 20,bottom: 20))
+        ),
+        onPressed: () {
+
+        },
+        child: Text("Continuar"),
+      ),
+    );
+  }
 }
